fa3ba836ba5469d1286d037ceba71dd9dd865535
neogithub <evan.buxton@neoscape.com>
2015-04-19 09:50:21 -0400

    Initial Commit

---
+++ thedistrict/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  thedistrict
+//
+//  Created by Evan Buxton on 4/19/15.
+//  Copyright (c) 2015 Evan Buxton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ thedistrict/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thedistrict
+//
+//  Created by Evan Buxton on 4/19/15.
+//  Copyright (c) 2015 Evan Buxton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
